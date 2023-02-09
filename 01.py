c8b01df729be3e64945b8dd50d3d2d5bc1d6e59a
sinugarc <115789430+sinugarc@users.noreply.github.com>
2023-02-09 09:36:47 +0100

    update 01

--- 01.py
+++ 01.py
@@ -3,6 +3,31 @@ from multiprocessing import Process, current_process, Value, Array
 N=8
 def task(common, tid, turn):
 	a=0
-	for i in range(100):
+	for i in range(10):
 		print(f'{tid}-{i}:Non-critical Section')
+		a += 1
+		print(f'{tid}-{i}:End of non-critical Section')
+		while turn.value!=tid:
+			pass
+		print(f'{tid}−{i}: Critical sectoin')
+		v = common.value + 1
+		print(f'{tid}−{i}: Inside critical sectoin') 
+		common.value = v
+		print(f'{tid}−{i}: End of critical sectin')
+		turn.value (tid + 1) % N		
 
+def main():
+	lp = []
+	common = Value('i', 0)
+	turn = Value('i', 0)
+	for tid in range(N):
+		lp.append(Process(target=task, args=(common, tid, turn)))
+	print (f"Valor inicial del contador {common.value}")
+	for p in lp:
+		p.start()
+	for p in lp:
+		p.join()
+	print (f"Valor final del contador {common.value}")
+	print ("fin")
+if __name__ == "__main__":
+	main()
